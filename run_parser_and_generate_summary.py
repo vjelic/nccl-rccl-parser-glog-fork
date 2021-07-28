04f0684c0d69c8a80b0672715db2383e4f62108e
Hubert Lu <55214931+hubertlu-tw@users.noreply.github.com>
2021-07-28 12:46:28 -0700

    Update run_parser_and_generate_summary.py
    Fixed HSA_FORCE_FINE_GRAIN_PCIE=1

--- run_parser_and_generate_summary.py
+++ run_parser_and_generate_summary.py
@@ -22,7 +22,7 @@ def main():
             sys.exit(1)
         
         os.system("cat net_unique.sh")
-        run_script_cmd = "sh net_unique.sh | tee rccl_perf_log.txt"
+        run_script_cmd = "HSA_FORCE_FINE_GRAIN_PCIE=1 sh net_unique.sh | tee rccl_perf_log.txt"
         if os.system(run_script_cmd):
             print ("ERROR: Unable to run rccl-tests properly.")
             sys.exit(1)
@@ -43,7 +43,7 @@ def main():
             sys.exit(1)
         
         os.system("cat net_unique.sh")
-        run_script_cmd = "HSA_FORCE_FINE_GRAIN_PCIE=1 sh net_unique.sh | tee nccl_perf_log.txt"
+        run_script_cmd = "sh net_unique.sh | tee nccl_perf_log.txt"
         if os.system(run_script_cmd):
             print ("ERROR: unable to run nccl-tests")
             sys.exit(1)
