a83cbfa6c80980a8196e2815918f07916b8106fb
Hubert Lu <55214931+hubertlu-tw@users.noreply.github.com>
2021-08-03 10:30:43 -0700

    Update run_parser_and_generate_summary.py

--- run_parser_and_generate_summary.py
+++ run_parser_and_generate_summary.py
@@ -6,7 +6,10 @@ def main():
     debug_log = os.path.abspath(args.nccl_debug_log)
     
     ## Generate a script to run nccl/rccl tests.
-    gen_cmd = "python rccl_nccl_parser.py --nccl-debug-log " + debug_log + " --output-script-name net --unique"
+    if args.new_log:
+        gen_cmd = gen_cmd = "python rccl_nccl_parser_new.py --nccl-debug-log " + debug_log + " --output-script-name net --unique --new-log"
+    else:
+        gen_cmd = "python rccl_nccl_parser_new.py --nccl-debug-log " + debug_log + " --output-script-name net --unique"
     if os.system(gen_cmd):
         print ("ERROR: Failed to parse the log.")
         sys.exit(1)
@@ -14,43 +17,47 @@ def main():
     ## change directory to rccl-tests/nccl-tests
     if args.rocm:
         rccl_tests_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "rccl-tests")
-        os.system("cp net_unique.sh " + rccl_tests_path)
-        #os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "rccl-tests"))
+        os.system("cp net_unique_topo.sh " + rccl_tests_path)
         os.chdir(rccl_tests_path)
         if os.system("./install.sh > /dev/null 2>&1"):
             print("ERROR: Failed to install rccl-tests.")
             sys.exit(1)
         
-        os.system("cat net_unique.sh")
-        run_script_cmd = "sh net_unique.sh | tee rccl_perf_log.txt"
+        os.system("cat net_unique_topo.sh")
+        run_script_cmd = "HSA_FORCE_FINE_GRAIN_PCIE=1 sh net_unique_topo.sh | tee topo_rccl_tests.txt"
         if os.system(run_script_cmd):
             print ("ERROR: Unable to run rccl-tests properly.")
             sys.exit(1)
-        os.system("mv rccl_perf_log.txt ../")
+        os.system("mv topo_rccl_tests.txt ../")
         os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
 
         print (os.getcwd())
-        summary_cmd = "python generate_summary.py --log-file rccl_perf_log.txt --script-file net_unique.sh --count-file net_counts.csv"
+        summary_cmd = "python generate_summary_new.py --log-file topo_rccl_tests.txt --output-file-name net_summary --count-file net_counts.csv"
+                
         os.system(summary_cmd)
         print ("INFO: Finished dumping all data.")
+        
+        
 
     if args.cuda:
         nccl_tests_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "nccl-tests")
-        os.system("cp net_unique.sh " + nccl_tests_path)
+        os.system("cp net_unique_topo.sh " + nccl_tests_path)
         os.chdir(nccl_tests_path)
-        if os.system("make > /dev/null 2>&1"):
-            print ("ERROR: Failed to install nccl-unit tests")
+        if os.system("./install.sh > /dev/null 2>&1"):
+            print("ERROR: Failed to install nccl-tests.")
             sys.exit(1)
         
-        os.system("cat net_unique.sh")
-        run_script_cmd = "sh net_unique.sh | tee nccl_perf_log.txt"
+        os.system("cat net_unique_topo.sh")
+        run_script_cmd = "HSA_FORCE_FINE_GRAIN_PCIE=1 sh net_unique_topo.sh | tee topo_rccl_tests.txt"
         if os.system(run_script_cmd):
-            print ("ERROR: unable to run nccl-tests")
+            print ("ERROR: Unable to run nccl-tests properly.")
             sys.exit(1)
-        os.system("mv nccl_perf_log.txt ../")
+        os.system("mv topo_rccl_tests.txt ../")
         os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
-        
-        summary_cmd = "python generate_summary.py --log-file nccl_perf_log.txt --script-file net_unique.sh --output-file-name nv_net_summary --count-file net_counts.csv"
+
+        print (os.getcwd())
+        summary_cmd = "python generate_summary_new.py --log-file topo_rccl_tests.txt --output-file-name net_summary --count-file net_counts.csv"
+                
         os.system(summary_cmd)
         print ("INFO: Finished dumping all data.")
 
@@ -60,6 +67,7 @@ if __name__ == '__main__':
                             help="NCCL/RCCL log after running app with NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=INIT,COLL")
     parser.add_argument("--rocm", action="store_true", default=False, help="Run the tests on ROCm using rccl-tests")
     parser.add_argument("--cuda", action="store_true", default=False, help="Run the tests on CUDA using nccl-tests")
+    parser.add_argument("--new-log", action="store_true", default=False, help="NCCL/RCCL log after running app with NCCL or RCCL 2.8 or below")
 
     args = parser.parse_args()
     main()
